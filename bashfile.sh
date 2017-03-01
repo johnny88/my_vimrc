e50f113e17b36f88be0a92c261e626a9d1a94ea0
John Pangalos <john@bobby.se>
2017-03-01 09:59:42 +0100

    Update bashfile.sh

--- bashfile.sh
+++ bashfile.sh
@@ -1,3 +1,4 @@
 # some usefull bash things
 sudo apt install gimp gimp-plugin-registry
 wget -qO- https://get.docker.com/ | sh
+sudo bash -c 'curl -L "https://github.com/docker/compose/releases/download/1.11.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose'
