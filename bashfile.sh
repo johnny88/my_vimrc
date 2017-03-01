a5d28ccd765d0421b46f1c81bf7674041a0052be
John Pangalos <john@bobby.se>
2017-03-01 10:05:31 +0100

    Update bashfile.sh

--- bashfile.sh
+++ bashfile.sh
@@ -2,3 +2,15 @@
 sudo apt install gimp gimp-plugin-registry
 wget -qO- https://get.docker.com/ | sh
 sudo bash -c 'curl -L "https://github.com/docker/compose/releases/download/1.11.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose'
+# 1. Add the Spotify repository signing key to be able to verify downloaded packages
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+
+# 2. Add the Spotify repository
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+# 3. Update list of available packages
+sudo apt-get update
+
+# 4. Install Spotify
+sudo apt-get install spotify-client
+        
