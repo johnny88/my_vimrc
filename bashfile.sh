40d75fa502fa1b019fd896f2ff3e2c3dae255632
John Pangalos <john@bobby.se>
2017-03-01 10:10:15 +0100

    Update bashfile.sh

--- bashfile.sh
+++ bashfile.sh
@@ -1,7 +1,6 @@
 # some usefull bash things
 sudo apt install gimp gimp-plugin-registry
-wget -qO- https://get.docker.com/ | sh
-sudo bash -c 'curl -L "https://github.com/docker/compose/releases/download/1.11.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose'
+
 # 1. Add the Spotify repository signing key to be able to verify downloaded packages
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 
@@ -13,4 +12,13 @@ sudo apt-get update
 
 # 4. Install Spotify
 sudo apt-get install spotify-client
-        
+
+#install docker and docker compose
+wget -qO- https://get.docker.com/ | sh
+sudo bash -c 'curl -L "https://github.com/docker/compose/releases/download/1.11.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose'
+#Add the docker group if it doesn't already exist:
+sudo groupadd docker
+#Add the connected user "${USER}" to the docker group. Change the user name to match your preferred user:
+sudo gpasswd -a ${USER} docker
+#Restart the Docker daemon:
+sudo service docker restart
