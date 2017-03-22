bb801211386051347d73392c3ce14156e9dc0117
John Pangalos <john@bobby.se>
2017-03-22 14:10:40 +0100

    Update bashfile.sh

--- bashfile.sh
+++ bashfile.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sudo apt-get install build-essential cmake
 sudo apt-get install python-dev python3-dev
-cd ~/.vim/bundle/YouCompleteMe
+cd ~/.vim/bundle/youcompleteme/
 ./install.py --all
 cd ~/
 
