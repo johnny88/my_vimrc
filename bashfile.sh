b4004bc3717035dcd6587545362fa78fad16feb4
John Pangalos <john@bobby.se>
2017-06-16 12:40:47 +0200

    Update bashfile.sh

--- bashfile.sh
+++ bashfile.sh
@@ -46,6 +46,10 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
 
+#install atom
+sudo add-apt-repository ppa:webupd8team/atom
+sudo apt update && sudo apt install atom
+
 #install zsh
 sudo apt install zsh
 #change default shell
